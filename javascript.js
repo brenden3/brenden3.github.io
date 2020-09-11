258e5314546dd0c887d143f61d1c3bf7b2a51793
veryyy <70784120+veryyy@users.noreply.github.com>
2020-09-11 13:27:36 -0700

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,43 @@
+//Make the DIV element draggagle:
+dragElement(document.getElementById("final-result"));
+
+function dragElement(elmnt) {
+
+    elmnt.onmousedown = dragMouseDown;
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    /* stop moving when mouse button is released:*/
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+function disappear() {
+document.getElementById("you").style.visibility = "hidden";
+}
+function appear() {
+document.getElementById("you").style.visibility = "visible";
+}
