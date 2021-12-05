0f07d930ea35601e170671e94a78a2a4f1fb4a8e
brenden <70784120+brenden3@users.noreply.github.com>
2021-12-04 18:51:16 -0800

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,11 @@
+function toggle() {
+var x = document.getElementById("cmd");
+var y = document.getElementById("caption");
+if (x.style.display === "none") {
+x.style.display = "block";
+y.innerHTML = "awesome code (click to close)";
+} else {
+x.style.display = "none";
+y.innerHTML = "awesome code (click to open)";
+}
+}
